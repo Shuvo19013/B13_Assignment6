51e2049e95c5b359edfcae7e9601d738577b995f
Shuvo19013 <shuvo15-5428@diu.edu.bd>
2026-04-01 23:28:49 +0600

    Added Navbar with cart icon and responsive layout

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,92 @@
+import React from "react";
+import navShoppingCart from "../assets/products/shopping-cart.png";
+
+const NavBar = ({ carts }) => {
+  return (
+    <div className="bg-base-100 shadow-sm">
+      <div className="navbar max-w-300 mx-auto">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />
+              </svg>
+            </div>
+            <ul
+              tabIndex="-1"
+              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+            >
+              <li>
+                <a className="font-semibold">Products</a>
+              </li>
+              <li>
+                <a className="font-semibold"> Features</a>
+              </li>
+              <li>
+                <a className="font-semibold">Pricing</a>
+              </li>
+              <li>
+                <a className="font-semibold">Testimonials</a>
+              </li>
+              <li>
+                <a className="font-semibold">FAQ</a>
+              </li>
+            </ul>
+          </div>
+          <a className="bg-linear-to-r from-[#4F39F6] to-[#9514FA] bg-clip-text text-transparent text-3xl font-bold">
+            DigiTools
+          </a>
+        </div>
+
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal px-1">
+            <li>
+              <a className="font-semibold">Products</a>
+            </li>
+            <li>
+              <a className="font-semibold"> Features</a>
+            </li>
+            <li>
+              <a className="font-semibold">Pricing</a>
+            </li>
+            <li>
+              <a className="font-semibold">Testimonials</a>
+            </li>
+            <li>
+              <a className="font-semibold">FAQ</a>
+            </li>
+          </ul>
+        </div>
+
+        <div className="navbar-end gap-4">
+          <div className="relative">
+            <img className="w-5 h-5" src={navShoppingCart} alt="cart" />
+
+            {carts.length > 0 && (
+              <span className="absolute -top-2 -right-2 bg-red-500 text-white text-xs w-5 h-5 flex items-center justify-center rounded-full">
+                {carts.length}
+              </span>
+            )}
+          </div>
+          <p>Login</p>
+          <a className="btn bg-linear-to-r from-[#4F39F6] to-[#9514FA] rounded-full text-white">
+            Get Started
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NavBar;
